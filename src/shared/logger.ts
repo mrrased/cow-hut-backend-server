7353eec50874d16c4c93f1fa12c8eff06f575cfc
mrrased <rashedtechdr.786@gmail.com>
2023-06-18 20:29:31 +0600

    logger ignore

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,73 +1,73 @@
-import path from 'path';
-import { createLogger, format, transports } from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
+// import path from 'path';
+// import { createLogger, format, transports } from 'winston';
+// import DailyRotateFile from 'winston-daily-rotate-file';
 
-const { combine, timestamp, label, printf, prettyPrint } = format;
+// const { combine, timestamp, label, printf, prettyPrint } = format;
 
-// Custom Format
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  const date = new Date(timestamp);
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+// // Custom Format
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   const date = new Date(timestamp);
+//   const hour = date.getHours();
+//   const minutes = date.getMinutes();
+//   const seconds = date.getSeconds();
 
-  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
-});
+//   return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
+// });
 
-const logger = createLogger({
-  level: 'info',
-  format: combine(
-    label({ label: 'COW' }),
-    timestamp(),
-    myFormat,
-    prettyPrint()
-  ),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(
-        // eslint-disable-next-line no-undef
-        process.cwd(),
-        'logs',
-        'winston',
-        'successes',
-        'Cow-%DATE%-success.log'
-      ),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
+// const logger = createLogger({
+//   level: 'info',
+//   format: combine(
+//     label({ label: 'COW' }),
+//     timestamp(),
+//     myFormat,
+//     prettyPrint()
+//   ),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         // eslint-disable-next-line no-undef
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'successes',
+//         'Cow-%DATE%-success.log'
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
 
-const errorLogger = createLogger({
-  level: 'error',
-  format: combine(
-    label({ label: 'COW' }),
-    timestamp(),
-    myFormat,
-    prettyPrint()
-  ),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(
-        // eslint-disable-next-line no-undef
-        process.cwd(),
-        'logs',
-        'winston',
-        'errors',
-        'Cow-%DATE%-error.log'
-      ),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
+// const errorLogger = createLogger({
+//   level: 'error',
+//   format: combine(
+//     label({ label: 'COW' }),
+//     timestamp(),
+//     myFormat,
+//     prettyPrint()
+//   ),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         // eslint-disable-next-line no-undef
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'errors',
+//         'Cow-%DATE%-error.log'
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
 
-export { logger, errorLogger };
+// export { logger, errorLogger };
